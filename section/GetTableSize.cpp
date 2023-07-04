28182cc0526068a0b15c07433cd71bcc7c8c4345
4z0t <gastec139@gmail.com>
2023-07-04 18:00:12 +0300

    WIP

--- section/GetTableSize.cpp
+++ section/GetTableSize.cpp
@@ -1,3 +1,4 @@
+#include "include/moho.h"
 void GetTableSize()
 {
     asm(
@@ -16,9 +17,9 @@ void GetTableSize()
         "MOV ECX,[EAX+0x14];"
         "L3:;"
         "CMP DWORD PTR [ECX+0x8],0x0;"
-        "JE L4;"
+        "JE L04;"
         "ADD EBX,0x1;"
-        "L4:;"
+        "L04:;"
         "ADD ECX,0x14;"
         "DEC EDX;"
         "JNZ L3;"
@@ -40,13 +41,12 @@ void GetTableSize()
         "SUB ESP,0x4;"
         "MOVSS [ESP],XMM0;"
         "PUSH ESI;"
-        "CALL 0x0090CD40;"     //PushNumber
+        "CALL 0x0090CD40;" // PushNumber
         "ADD ESP,0x8;"
         "MOV EAX,0x1;"
         "RET;"
         "Err:;"
-        "XOR EAX,EAX;"
-    );
+        "XOR EAX,EAX;");
 }
 
 void IsTableEmpty()
@@ -88,16 +88,85 @@ void IsTableEmpty()
         "PUSH 0x0;"
         "L12:;"
         "PUSH ESI;"
-        "CALL 0x0090CF80;"     //PushBool
+        "CALL 0x0090CF80;" // PushBool
         "ADD ESP,0x8;"
-        "MOV EAX,0x1;"
-    );
+        "MOV EAX,0x1;");
 }
 
-int RegTableFuncsDesc[] = {"getsize2",&GetTableSize,
-                           "empty2",&IsTableEmpty,
-                           "getn2",0x00927C20,
-                           0,0};
+void lua_createtable(lua_State *l, int narr, int nhash)
+{
+    asm(
+                "push    esi;"
+                "mov     esi, [esp+4+arg_0];"
+                "mov     eax, [esi+10h];"
+                "mov     ecx, [eax+2Ch];"
+                "cmp     ecx, [eax+24h];"
+                "push    edi;"
+                "jb      short loc_90D130;"
+                "cmp     dword ptr [eax+28h], 0;"
+                "jnz     short loc_90D130;"
+                "push    esi;"
+                "call    sub_915D90;"
+                "add     esp, 4;"
+                "loc_90D130: ;"
+                "mov     edi, [esi+8];"
+                "push    0;" //nhash
+                "push    0;" //narr
+                "push    esi;"
+                "call    0x00927320;" //luaH_new
+                "movzx   edx, byte ptr [eax+4];"
+                "mov     [edi], edx;"
+                "mov     [edi+4], eax;"
+                "mov     eax, [esi+8];"
+                "mov     ecx, [esi+14h];"
+                "add     esp, 0Ch;"
+                "cmp     eax, [ecx+4];"
+                "jb      short loc_90D173;"
+                "mov     edx, [esi+18h];"
+                "sub     edx, eax;"
+                "mov     edi, 8;"
+                "cmp     edx, edi;"
+                "jg      short loc_90D16D;"
+                "push    1;"
+                "push    esi;"
+                "call    sub_913990;"
+                "add     esp, 8;"
+                "loc_90D16D:                            ;"
+                "add     [esi+8], edi;"
+                "pop     edi;"
+                "pop     esi;"
+                "retn;"
+                "loc_90D173:                           ;"
+                "add     dword ptr [esi+8], 8;"
+                "pop     edi;"
+                "pop     esi;"
+                "retn;"
+    );
+           
+}
+
+int _CreateTable(lua_State *l)
+{
+    int narr = luaL_checknumber(l, 1);
+    int nhash = luaL_checknumber(l, 2);
+    lua_createtable(l, narr, nhash);
+                
+
+    // LuaState *ls = l->LuaState;
+    // LuaObject tbl;
+    // LuaStackObject so;
+    // CLuaObject::CLuaObject2(&tbl, ls);
+    // CLuaObject::AssignNewTable(&tbl, ls, narr, nhash);
+    // CLuaObject::PushStack(&tbl, &so, ls);
+    // CLuaObject::DLuaObject(&tbl);
+    return 1;
+}
+
+int RegTableFuncsDesc[] = {"getsize2", &GetTableSize,
+                           "empty2", &IsTableEmpty,
+                           "getn2", 0x00927C20,
+                           "create", &_CreateTable,
+                           0, 0};
 
 void RegTableFuncs()
 {
@@ -108,7 +177,6 @@ void RegTableFuncs()
         "CALL 0x0090DE00;"
         "JMP 0x009283B6;"
         :
-        : [RegTableFuncsDesc] "i" (RegTableFuncsDesc)
-        :
-    );
+        : [RegTableFuncsDesc] "i"(RegTableFuncsDesc)
+        :);
 }
