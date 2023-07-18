850c616f90c30eb100b00f4585157c87d65458a1
4z0t <gastec139@gmail.com>
2023-07-18 18:12:25 +0300

    wip setstat bruuuuh

--- section/SetStatFix.cpp
+++ section/SetStatFix.cpp
@@ -14,9 +14,26 @@ void SetStatCheck()
         "mov     eax, [edi];" // pointer to lua state
         "call %[CheckPointer];"
         "cmp ebx, 0;"
-        "jne 0x6CCA2C;" // jump at "mov     esi, 3"
-
-        "jmp 0x6CCA9B;"//go to return 1
+        "je 0x6CCA9B;"//go to return 1
+         "mov     esi, 3;"
+         "push    esi;"
+         "push    eax;"
+         "call    0x90C740;"//lua_type;"
+         "add     esp, 8;"
+         "cmp     eax, esi;"
+         "push    esi;"
+         "jnz     0x6CCAAF;"
+         "mov     [esp+0x54-0x44], edi;"
+         "mov     [esp+0x54-0x40], esi;"
+         "mov     ecx, [edi];"
+         "push    ecx;"
+         "call    0x90C740;"//lua_type;"
+         "add     esp, 8;"
+         "cmp     eax, esi;"
+         "jz      0x6CCA65;"
+         "push    offset 0xE01168;"
+         "lea     ecx, [esp+0x54-0x44];"
+        "jmp 0x6CCA60;"  //LuaStackObjectTypeError
         :
         : [CheckPointer] "i"(CheckPointer)
         :
