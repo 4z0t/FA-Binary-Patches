3b49b16bbca9a559f43442983b5a941434e00502
4z0t <gastec139@gmail.com>
2023-08-07 13:38:35 +0300

    wip

--- section/DrawFunc.cpp
+++ section/DrawFunc.cpp
@@ -4,8 +4,8 @@ void _DrawRect()
 /*
     eax vec3_1
     ecx vec3_2
-    edi int
-    xmm0 float_1
+    edi int color?
+    xmm0 float_1 //thickness
     stack batcher
     stack vec3_3
     stack heightmap
@@ -13,7 +13,6 @@ void _DrawRect()
 */
 {
     asm(
-        "offset = 0x04;"
         "push ebp;"
         "mov ebp, esp;"
         "push ecx;"
@@ -21,7 +20,7 @@ void _DrawRect()
         "mov ecx,   [ebp + 0x04 + 0x08];" // vec3_2
         "mov edi,   [ebp + 0x04 + 0x0C];" // int
         "movss xmm0,[ebp + 0x04 + 0x10];" // float_1
-        "mov eax,   [ebp + 0x04 + 0x20];"  // float_2
+        "mov eax,   [ebp + 0x04 + 0x20];" // float_2
         "push eax;"
         "mov eax,   [ebp + 0x04 + 0x1C];" // heightmap
         "push eax;"
@@ -40,32 +39,68 @@ void _DrawRect()
 inline int DrawRect(
     float *v1,
     float *v2,
-    int i,
+    unsigned int i,
     float f1,
     int *batcher,
     float *v3,
-    int *heightmap,
+    void *heightmap,
     float f2)
 {
-    return reinterpret_cast<int (*)(float *, float *, int, float, int *, float *, int *, float)>(_DrawRect)(v1, v2, i, f1, batcher, v3, heightmap, f2);
+    return reinterpret_cast<int (*)(float *, float *, unsigned int, float, int *, float *, void *, float)>(_DrawRect)(v1, v2, i, f1, batcher, v3, heightmap, f2);
 }
+
+//#define ui_Terrain GDecl(0x010A6438, void *)
+#define ui_WldMap GDecl(0x010C4F58, void *)
+
+void FlushBatcher(void *batcher)
+{
+    reinterpret_cast<void (*)(void *)>(0x0043A140)(batcher);
+    asm("add esp, 4;");
+}
+
+namespace IWldTerrainRes
+{
+    void *GetWldMap()
+    {
+        return (void *)g_CWldSession->map;
+    }
+
+    void *GetTerrainRes(void *wldmap)
+    {
+        return (void *)*((int *)wldmap + 1);
+    }
+
+    void *GetMap(void *wldTerrainRes)
+    {
+        return (void *)*((int *)wldTerrainRes + 1);
+    }
+
+} // namespace  IWldTerrainRes
+
 // UI_Lua DrawRect()
 int LuaDrawRect(lua_State *l)
 {
     LuaState *ls = l->LuaState;
 
     //   int *batcher = (int*)(0x112D6140);//(int *)(g_WRenViewport + 2135);
-    int **batcher = (int **)(((int *)g_WRenViewport) + 2135);
+    int *batcher = *(int **)(((int *)g_WRenViewport) + 2135);
     if (batcher == nullptr)
     {
         WarningF("%s", "LOX");
         return 0;
     }
-    LogF("%p", batcher);
-    LogF("%p", *batcher);
-    float a[]{0, 0, 0};
-    float b[]{255, 255, 0};
-    float c[]{0, 255, 0};
-    DrawRect(a, b, -65281, 5.0f, *batcher, c, 0, -1000.0f);
+    // LogF("%p", batcher);
+    // LogF("%p", *batcher);
+    float a[]{0, 0, 8};
+    float b[]{8, 0, 0};
+    float c[]{653.5, 18.77, 168.5};
+    void *wldmap = IWldTerrainRes::GetWldMap();
+    void *terrain = IWldTerrainRes::GetTerrainRes(wldmap);
+    if (!terrain)
+        return 0;
+    void *map = IWldTerrainRes::GetMap(terrain);
+    LogF("%s", "Here!");
+    DrawRect(a, b, 0xFF00FF00, 0.03, batcher, c, map, 17.5);
+    // FlushBatcher(batcher);
     return 0;
 }
