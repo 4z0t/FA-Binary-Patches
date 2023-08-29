96cdabc488ad8577c8e9ac7ea430260ea5308ce2
4z0t <gastec139@gmail.com>
2023-08-29 12:42:07 +0300

    add comments

--- hooks/BitmapColorMask.cpp
+++ hooks/BitmapColorMask.cpp
@@ -1,8 +1,9 @@
 #include "../define.h"
 asm(
+    //prevent set alpha from resetting color mask
     ".section h0; .set h0,0x789988;"
     "call "QU(_SetAlpha)";"
-
+    //check for bool not type != 0
     ".section h1; .set h1,0x789972;"
     "call 0x90CA40;" //call lua_toboolean
 
