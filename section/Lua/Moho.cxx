23b39d89439b9ce3332474f322c1f0b36d6befba
4z0t <gastec139@gmail.com>
2024-10-04 20:17:44 +0300

    replace with lua_getglobal

--- section/Lua/Moho.cxx
+++ section/Lua/Moho.cxx
@@ -4,8 +4,7 @@ LuaObject SCR_Import(LuaState *state, const char *name)
 {
     lua_State *L = state->m_state;
     int top = lua_gettop(L);
-    lua_pushstring(L, "import");
-    lua_gettable(L, LUA_GLOBALSINDEX);
+    lua_getglobal(L, "import");
     lua_pushstring(L, name);
     lua_call(L, 1, 1);
     LuaObject obj{state, -1};
