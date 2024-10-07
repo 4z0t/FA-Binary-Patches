655b983a52860cc0b271e3e5ed5a11832a733996
4z0t <gastec139@gmail.com>
2024-10-07 22:13:43 +0300

    Update Selection.cxx

--- section/Selection/Selection.cxx
+++ section/Selection/Selection.cxx
@@ -20,55 +20,53 @@ void __stdcall HandleNewSelection(Moho::CWldSession *session,
     }
 
     auto ls = session->state;
+    LuaObject units_list;
+    units_list.AssignNewTable(ls, size, 0);
+
+    int j = 1;
+    for (MapNode *node : *new_selection)
     {
-        LuaObject units_list;
-        units_list.AssignNewTable(ls, size, 0);
+        void *value = node->value;
+        if (value == nullptr)
+            continue;
+        Moho::UserUnit *uunit = (Moho::UserUnit *)((char *)value - 8);
+        if (uunit == nullptr)
+            continue;
 
-        int j = 1;
-        for (MapNode *node : *new_selection)
+        LuaObject obj;
+        GetIUnitVTable(uunit)->GetLuaObject(Offset<Moho::Unit_ *>(uunit, 0x148), &obj);
+        units_list.SetObject(j, obj);
+        j++;
+    }
+    try
+    {
+        lua_State *L = ls->m_state;
+        LuaObject f = SCR_Import(ls, "/lua/ui/game/gamemain.lua")["HandleSelection"];
+        f.PushStack(L);
+        units_list.PushStack(L);
+        lua_call(L, 1, 1);
+
+        LuaObject result{ls, -1};
+        lua_pop(L, 1);
+
+        if (!result.IsTable())
         {
-            void *value = node->value;
-            if (value == nullptr)
-                continue;
-            Moho::UserUnit *uunit = (Moho::UserUnit *)((char *)value - 8);
-            if (uunit == nullptr)
-                continue;
-
-            LuaObject obj;
-            GetIUnitVTable(uunit)->GetLuaObject(Offset<Moho::Unit_ *>(uunit, 0x148), &obj);
-            units_list.SetObject(j, obj);
-            j++;
+            ls->Error("Expected table");
         }
-        try
+
+        new_selection->~UserUnitMap();
+        new (new_selection) UserUnitMap();
+
+        for (const auto &[key, value] : IPairs(result))
         {
-            lua_State *L = ls->m_state;
-            LuaObject f = SCR_Import(ls, "/lua/ui/game/gamemain.lua")["HandleSelection"];
-            f.PushStack(L);
-            units_list.PushStack(L);
-            lua_call(L, 1, 1);
+            UserUnit *u = UserUnitFromObj(&value, ls);
 
-            LuaObject result{ls, -1};
-            lua_pop(L, 1);
-
-            if (!result.IsTable())
-            {
-                ls->Error("Expected table");
-            }
-
-            new_selection->~UserUnitMap();
-            new (new_selection) UserUnitMap();
-
-            for (const auto &[key, value] : IPairs(result))
-            {
-                UserUnit *u = UserUnitFromObj(&value, ls);
-
-                UserUnitMap_AddResult r;
-                UserUnitMap_Add(&r, new_selection, u);
-            }
+            UserUnitMap_AddResult r;
+            UserUnitMap_Add(&r, new_selection, u);
         }
-        catch (const std::exception &e)
-        {
-            WarningF("%s", e.what());
-        }
+    }
+    catch (const std::exception &e)
+    {
+        WarningF("%s", e.what());
     }
 }
