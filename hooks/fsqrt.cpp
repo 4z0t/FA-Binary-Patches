bbae954a6ffda82ba8a69b8ae48d2f415d891500
4z0t <gastec139@gmail.com>
2024-10-10 12:51:45 +0300

    comment

--- hooks/fsqrt.cpp
+++ hooks/fsqrt.cpp
@@ -1,15 +1,15 @@
 #include "../asm.h"
 
 asm(
-    SECTION(0, 0x00452FC0)
+    SECTION(0, 0x00452FC0) // sqrt
     "fld dword ptr [esp+4];"
     "fsqrt;"
     "ret;"
-    SECTION(1, 0x0040EAF0)
+    SECTION(1, 0x0040EAF0) // sqrt
     "fld dword ptr [esp+4];"
     "fsqrt;"
     "ret;"
-    SECTION(2, 0x0050D150)
+    SECTION(2, 0x0050D150) // inverse sqrt
     "fld dword ptr [esp+4];"
     "fsqrt;"
     "fld1;"
