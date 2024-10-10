0af5f5400487148ffdedb620dff2667f650a8118
4z0t <gastec139@gmail.com>
2024-10-10 21:29:31 +0300

    wip

--- hooks/OnCreateInitLuaState.cpp
+++
@@ -1,5 +0,0 @@
-#include "../define.h"
-asm(
-  ".section h0; .set h0, 0x459EE3;"
-  "call "QU(OnCreateInitLuaState)";"
-);
--- hooks/selectionPriority.cpp
+++
@@ -1,6 +0,0 @@
-#include "../define.h"
-asm(
-  ".section h0; .set h0,0x863C13;"
-  "jmp "QU(selectionPriority)";"
-  "nop;"
-);
