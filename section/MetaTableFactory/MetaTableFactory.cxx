2ad72c76d73017d97cc47c9480131ea15173036c
4z0t <gastec139@gmail.com>
2024-10-23 19:06:01 +0300

    move to cxx

--- section/MetaTableFactory/MetaTableFactory.cxx
+++ section/MetaTableFactory/MetaTableFactory.cxx
@@ -1,6 +1,29 @@
 #include "MetaTableFactory.h"
 #include "magic_classes.h"
 
+class TestMetaTable : public MetaTableFactory
+{
+    static int Index(lua_State *L)
+    {
+        LogF("index");
+        return 0;
+    }
+
+    static int NewIndex(lua_State *L)
+    {
+        LogF("newindex");
+        return 0;
+    }
+
+    virtual LuaObject *Create(LuaObject *metatable, LuaState *state) const override
+    {
+        new (metatable) LuaObject();
+        metatable->AssignNewTable(state, 0, 0);
+        metatable->Register("__newindex", &NewIndex, 0);
+        metatable->Register("__index", &Index, 0);
+        return metatable;
+    }
+};
 const TestMetaTable testmeta{};
 
 int TEST_METATABLE(lua_State *l)
--- section/MetaTableFactory/MetaTableFactory.h
+++ section/MetaTableFactory/MetaTableFactory.h
@@ -29,27 +29,3 @@ public:
 
     const int index;
 };
-
-class TestMetaTable : public MetaTableFactory
-{
-    static int Index(lua_State *L)
-    {
-        LogF("index");
-        return 0;
-    }
-
-    static int NewIndex(lua_State *L)
-    {
-        LogF("newindex");
-        return 0;
-    }
-
-    virtual LuaObject *Create(LuaObject *metatable, LuaState *state) const override
-    {
-        new (metatable) LuaObject();
-        metatable->AssignNewTable(state, 0, 0);
-        metatable->Register("__newindex", &NewIndex, 0);
-        metatable->Register("__index", &Index, 0);
-        return metatable;
-    }
-};
