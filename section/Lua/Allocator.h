2133acf2d60802445c989b1de68c4c38f43468d6
Alexander <gastec139@gmail.com>
2025-12-31 11:41:54 +0300

    add lua_getdefaultmemoryfunctions

--- section/Lua/Allocator.h
+++ section/Lua/Allocator.h
@@ -1,7 +1,7 @@
 #include "LuaAPI.h"
 
-void *__cdecl luaHelper_ReallocFunction(void *ptr, unsigned int oldsize, unsigned int size, void *data, const char *allocName, unsigned int flags) asm("0x00923F20");
-void __cdecl luaHelper_FreeFunction(void *ptr, unsigned int oldsize, void *data) asm("0x00923F40");
+void *__cdecl luaHelper_ReallocFunction(void *ptr, unsigned int oldsize, unsigned int size, void *data, const char *allocName, unsigned int flags) asm("0x923F20");
+void __cdecl luaHelper_FreeFunction(void *ptr, unsigned int oldsize, void *data) asm("0x923F40");
 
 typedef void *(__cdecl *lua_ReallocFunction)(void *ptr, unsigned int oldsize, unsigned int size, void *data, const char *allocName, unsigned int flags);
 typedef void(__cdecl *lua_FreeFunction)(void *ptr, unsigned int oldsize, void *data);
@@ -9,4 +9,6 @@ typedef void(__cdecl *lua_FreeFunction)(void *ptr, unsigned int oldsize, void *d
 void __cdecl lua_setdefaultmemoryfunctions(
     lua_ReallocFunction reallocFunc,
     lua_FreeFunction freeFunc,
-    void *data) asm("0x00923F80");
+    void *data) asm("0x923F80");
+
+void __cdecl lua_getdefaultmemoryfunctions(lua_ReallocFunction *reallocFunc, lua_FreeFunction *freeFunc, void **data) asm("0x923F50");
