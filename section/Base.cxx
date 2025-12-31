90dcddd6ad07ede497cab5451da9b4469706c1d9
Alexander <gastec139@gmail.com>
2025-12-31 12:06:18 +0300

    add nothrow version

--- section/Base.cxx
+++ section/Base.cxx
@@ -1,5 +1,16 @@
 #include "global.h"
 #include "Vector.h"
+#include <new>
+
+void *operator new(std::size_t n, const std::nothrow_t &tag) noexcept
+{
+    return malloc(n);
+}
+
+void *operator new[](std::size_t n, const std::nothrow_t &tag) noexcept
+{
+    return malloc(n);
+}
 
 void *operator new(std::size_t n) noexcept(false)
 {
