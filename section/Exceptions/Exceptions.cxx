076a5792841aeaa00a02c7e07323855394f86008
Alexander <gastec139@gmail.com>
2026-01-05 16:32:48 +0300

    wip

--- section/Exceptions/Exceptions.cxx
+++ section/Exceptions/Exceptions.cxx
@@ -8,34 +8,37 @@
 //     bool _DoFree;
 // };
 
-extern "C" void __cdecl __std_exception_copy(
-    __std_exception_data const *_From,
-    __std_exception_data *_To)
+extern "C"
 {
-    _To->_DoFree = _From->_DoFree;
-    const char *_What = _From->_What;
-    if (!_To->_DoFree)
+    void __cdecl __std_exception_copy(
+        __std_exception_data const *_From,
+        __std_exception_data *_To)
     {
-        _To->_What = _What;
+        _To->_DoFree = _From->_DoFree;
+        const char *_What = _From->_What;
+        if (!_To->_DoFree)
+        {
+            _To->_What = _What;
+        }
+        else if (_What)
+        {
+            size_t len = strlen(_What) + 1;
+            _To->_What = (char *)malloc(len);
+            if (_To->_What)
+                strcpy_s((char *)_To->_What, len, _From->_What);
+        }
+        else
+        {
+            _To->_What = nullptr;
+        }
     }
-    else if (_What)
-    {
-        size_t len = strlen(_What) + 1;
-        _To->_What = (char *)malloc(len);
-        if (_To->_What)
-            strcpy_s((char *)_To->_What, len, _From->_What);
-    }
-    else
-    {
-        _To->_What = nullptr;
-    }
-}
 
-extern "C" void __cdecl __std_exception_destroy(
-    __std_exception_data *_Data)
-{
-    if (_Data->_DoFree)
+    void __cdecl __std_exception_destroy(
+        __std_exception_data *_Data)
     {
-        free((void*)_Data->_What);
+        if (_Data->_DoFree)
+        {
+            free((void *)_Data->_What);
+        }
     }
 }
