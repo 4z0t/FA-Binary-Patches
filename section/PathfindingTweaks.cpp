812f513568e46c56da41b710c2c71ed05f0e4c3f
Alexander <gastec139@gmail.com>
2026-02-09 00:16:05 +0300

    inline the patch

--- section/PathfindingTweaks.cpp
+++ section/PathfindingTweaks.cpp
@@ -4,19 +4,7 @@
 #include "global.h"
 #define NON_GENERAL_REG(var_) [var_] "g"(var_)
 
-float navPersonalPosMaxDistance = 50.0;
-
-volatile void asm__PersonalPositioningDistanceCheck()
-{
-    asm(
-        "fld %[navPersonalPosMaxDistance];"
-        "jmp 0x005AF8DB;"
-        
-        :
-        : NON_GENERAL_REG(navPersonalPosMaxDistance)
-        : 
-    );
-}
+SHARED float navPersonalPosMaxDistance = 50.0;
 
 int SetNavIndividualPosMaxDistance(lua_State *L) {
     navPersonalPosMaxDistance = luaL_optnumber(L, 1, 50.0);
