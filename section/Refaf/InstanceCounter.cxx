0303e64cd16d469b98ada3ec3d7f221cda073dbe
Alexander <gastec139@gmail.com>
2026-02-28 16:11:23 +0300

    add testing code

--- section/Refaf/InstanceCounter.cxx
+++ section/Refaf/InstanceCounter.cxx
@@ -1,1 +1,9 @@
 #include "InstanceCounterGetStatItem.h"
+#include "magic_classes.h"
+
+UIRegFunc __s{
+    "stat", "", [](lua_State *L) -> int
+    {
+        lua_pushnumber(L, Refaf::InstanceCounter<Refaf::CTask>::GetStatItem()->counter);
+        return 1;
+    }};
