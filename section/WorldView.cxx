375757f1fccb1d7bd46a7a3161d7a97bf3d945a8
Alexander <gastec139@gmail.com>
2026-03-06 14:34:19 +0300

    split implemenataion

--- section/WorldView.cxx
+++ section/WorldView.cxx
@@ -8,10 +8,9 @@ float Dot(const Vector4f &a, const Vector4f &b)
     return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
 }
 
-Vector2f Project(const float *camera, const Vector3f &v)
+Vector2f Project(const float *camera, const Vector3f &v, float left, float right, float top, float bottom)
 {
     const VMatrix4 &viewProjection = GetField<const VMatrix4>(camera, 0x9C);
-    const VMatrix4 &viewport = GetField<const VMatrix4>(camera, 0x284);
     Vector4f v0 = {v.x, v.y, v.z, 1.0f};
     Vector4f v1 = {viewProjection.d[0].d[0], viewProjection.d[1].d[0], viewProjection.d[2].d[0], viewProjection.d[3].d[0]};
     Vector4f v2 = {viewProjection.d[0].d[1], viewProjection.d[1].d[1], viewProjection.d[2].d[1], viewProjection.d[3].d[1]};
@@ -19,9 +18,19 @@ Vector2f Project(const float *camera, const Vector3f &v)
 
     float inv = Dot(v0, v3);
     return {
-        .x = viewport.d[3].d[2] * (Dot(v0, v1) / inv + 1.0f) * 0.5f + viewport.d[3].d[0],
-        .y = -viewport.d[3].d[3] * (Dot(v0, v2) / inv + 1.0f) * 0.5f + viewport.d[3].d[3] + viewport.d[3].d[1],
+        .x = (right - left) * (Dot(v0, v1) / inv + 1.0f) * 0.5f + left,
+        .y = (bottom - top) * (Dot(v0, v2) / inv + 1.0f) * 0.5f + top,
     };
+}
+
+Vector2f Project(const float *camera, const Vector3f &v)
+{
+    const VMatrix4 &viewport = GetField<const VMatrix4>(camera, 0x284);
+    return Project(camera, v,
+                   viewport.d[3].d[0],
+                   viewport.d[3].d[2] + viewport.d[3].d[0],
+                   viewport.d[3].d[3] + viewport.d[3].d[1],
+                   viewport.d[3].d[1]);
 }
 
 void ProjectVectors(lua_State *l, int index, float *camera)
