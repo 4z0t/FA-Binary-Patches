068d6e9dd2eae59ce5ffcf69afa7d5e0b41b6aff
Alexander <gastec139@gmail.com>
2026-03-15 13:15:40 +0300

    cleanup

--- section/Init.cpp
+++ section/Init.cpp
@@ -1,11 +1,4 @@
 #include "moho.h"
-struct A
-{
-    A()
-    {
-        WarningF("AAAAAAAAAAAAAAAA");
-    }
-} aaaaaaaaaaaaaaaaaaaa; // generate .text.startup
 
 typedef void (*global_ctor)();
 extern const global_ctor FIRST_CTOR;
